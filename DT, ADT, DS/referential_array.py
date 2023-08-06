f5a1be713b67b4ca1ccc746c55cf55b92ca49904
Aditya Patel <Adityap902021@gmail.com>
2023-08-06 19:16:20 +1000

    ADT

--- DT, ADT, DS/referential_array.py
+++ DT, ADT, DS/referential_array.py
@@ -1,26 +1,5 @@
 from __future__ import annotations
 
-""" Basic class implementation of an array of references for FIT units
-
-The code for the init function is a bit cryptic, so I explain it here in
-detail. The instance variables holding the physical array is constructed
-using the ctypes library to create a py_object (an object that can hold
-a reference to any python object). Note that for each value of length we
-have that (length * ctypes.py_object) is a type (e.g., if length=5, it
-would be a type called py_object_Array_5). Then (length *
-ctypes.py_object)() is equivalent to the initialisation in MIPS of the
-space to hold the references.
-
-Note that while I do check the precondition in __init__ (noone else
-would), I do not check that of getitem or setitem, since that is already
-checked by self.array[index].
-"""
-__author__ = """
-Julian Garcia for the __init__ code, Maria Garcia de la Banda for the rest.
-Modified by Jackson Goerner
-"""
-__docformat__ = "reStructuredText"
-
 from ctypes import py_object
 from typing import TypeVar, Generic
 
